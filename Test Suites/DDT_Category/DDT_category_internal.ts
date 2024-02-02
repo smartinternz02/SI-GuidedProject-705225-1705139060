5ae54947746327c7ef8010c66a9158dcddbdab0a
SDC <kumarram95698@gmail.com>
2024-02-03 00:24:18 +0530

    cross bowser testing , handling input text field ,git + jenkins integration with test listeners

---
+++ Test Suites/DDT_Category/DDT_category_internal.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DDT_category_internal</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ad48f1fd-1191-482a-b478-d657e91057b7</testSuiteGuid>
+   <testCaseLink>
+      <guid>a0418a14-af14-46a7-833d-99654251a765</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AZ_Homepage/DDT_internal_002</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>26517bc0-31a4-4872-a54a-f716100f7df6</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/DDT_category/DDT_category_internal data</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>26517bc0-31a4-4872-a54a-f716100f7df6</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Category</value>
+         <variableId>e655f850-b035-4ce9-8186-7dffd95ca743</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>26517bc0-31a4-4872-a54a-f716100f7df6</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Items</value>
+         <variableId>cd27e6b9-04bb-454f-9ee5-d68ee59f8751</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/cross_browser_001.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>cross_browser_001</name>
+   <tag></tag>
+   <delayBetweenInstances>0</delayBetweenInstances>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/DDT_Category/DDT_category_internal</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Firefox</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/Test_listeners/Test_listeners_Amazon</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
